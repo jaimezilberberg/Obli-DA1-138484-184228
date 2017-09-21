44aab3e769dcc4199db4262b840aad90e058276a
MacJaime <jaimezilberberg@gmail.com>
2017-09-21 20:33:39 -0300

    Add constructors

--- SchoolMangementORT/SchoolManagement/Person.cs
+++ SchoolMangementORT/SchoolManagement/Person.cs
@@ -4,5 +4,17 @@
     {
         public string Name{ get; set; }
         public string Surname { get; set; }
+
+        public Person()
+        {
+            Name = "";
+            Surname = "";
+        }
+
+        public Person(string name,string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
     }
 }
