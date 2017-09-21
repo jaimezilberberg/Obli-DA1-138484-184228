6507af8466388a092bf63231a169b32df6dba39f
MacJaime <jaimezilberberg@gmail.com>
2017-09-21 20:41:06 -0300

    TDD Student : Test TestMethodStudent

--- SchoolMangementORT/UnitTestProjectSchoolManagement/UnitTest2.cs
+++ SchoolMangementORT/UnitTestProjectSchoolManagement/UnitTest2.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProjectSchoolManagement
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest2
     {
         [TestMethod]
         public void TestMethod1()
---
+++ SchoolMangementORT/UnitTestProjectSchoolManagement/UnitTestStudent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolManagement;
+namespace UnitTestProjectSchoolManagement
+{
+    [TestClass]
+    public class UnitTestStudent
+    {
+        [TestMethod]
+        public void TestMethodStudent()
+        {
+            Person student = new Student();
+            student.StudentNumber = 1;
+            student.ID = 1;
+
+            Assert.AreEqual("",student.Name);
+            Assert.AreEqual("",student.Surname);
+            Assert.Equals(1, student.StudentNumber);
+            Assert.Equals(1, student.ID);
+        }
+    }
+}
